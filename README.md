9bfae4d1cc199e33bb4e0ea42c0999ae8ffba8c2
Maurice van Creij <maurice.vancreij@webqem.com>
2013-08-22 10:22:37 +1000

    Switched to constructors and polyfills

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Counts down to a future date using text and class names to add styling to.
 
-Try the <a href="http://www.woollymittens.nl/useful/default.php?url=countdown">countdown demo</a>.
+Try the <a href="http://www.woollymittens.nl/useful/default.php?url=countdown">demo</a>.
 
 ## How to use the script
 
@@ -15,7 +15,7 @@ The stylesheet is best included in the header of the document.
 This include can be added to the header or placed inline before the script is invoked.
 
 ```html
-<script src="./js/useful.countdown.js"></script>
+<script src="./js/countdown.min.js"></script>
 ```
 
 To enable the use of HTML5 tags in Internet Explorer 8 and lower, include *html5.js*. To provide an alternative for *document.querySelectorAll* and CSS3 animations in Internet Explorer 8 and lower, include *jQuery*.
@@ -27,59 +27,39 @@ To enable the use of HTML5 tags in Internet Explorer 8 and lower, include *html5
 <![endif]-->
 ```
 
-### Using vanilla JavaScript
+## How to start the script
 
 This is the safest way of starting the script, but allows for only one target element at a time.
 
 ```javascript
-var parent = documentGetElementById('id');
-useful.countdown.start(parent, {
+var countdown = new useful.Countdown( document.getElementById('id'), {
 	'end' : new Date('December 24, 2013 23:59:00'),
 	'onEnd' : function(){}
 });
+countdown.start();
 ```
 
 **id : {string}** - The ID attribute of an element somewhere in the document.
-
-**parent : {DOM node}** - The DOM element around which the functionality is centred.
 
 **end : {date}** - A date object containing the end date and time.
 
 **onEnd : {function}** - A function that will be executed when the time limit expires.
 
-### Using document.querySelectorAll
+## How to control the script
 
-This method allows CSS Rules to be used to apply the script to one or more nodes at the same time.
-
-```javascript
-useful.css.select({
-	rule : '#countdown',
-	handler : useful.countdown.start,
-	data : {
-		'end' : new Date('December 24, 2013 23:59:00'),
-		'onEnd' : function(){}
-	}
-});
-```
-
-**rule : {string}** - The CSS Rule for the intended target(s) of the script.
-
-**handler : {function}** - The public function that starts the script.
-
-**data : {object}** - Name-value pairs with configuration data.
-
-### Using jQuery
-
-This method is similar to the previous one, but uses jQuery for processing the CSS rule.
+### Update
 
 ```javascript
-$('#countdown').each(function (index, element) {
-	useful.countdown.start(element, {
-		'end' : new Date('December 24, 2013 23:59:00'),
-		'onEnd' : function(){}
-	});
-});
+cropper.update();
 ```
+
+Update the clock.
+
+## Prerequisites
+
+To concatenate and minify the script yourself, the following prerequisites are required:
++ https://github.com/WoollyMittens/useful-polyfills
 
 ## License
 This work is licensed under a Creative Commons Attribution 3.0 Unported License. The latest version of this and other scripts by the same author can be found at http://www.woollymittens.nl/
+
---
+++ src/useful.countdown.js
@@ -0,0 +1,89 @@
+/*
+	Source:
+	van Creij, Maurice (2012). "useful.countdown.js: A simple countdown timer.", version 20120606, http://www.woollymittens.nl/.
+
+	License:
+	This work is licensed under a Creative Commons Attribution 3.0 Unported License.
+*/
+
+(function (useful) {
+
+	// invoke strict mode
+	"use strict";
+
+	// private functions
+	useful.Countdown = function (obj, cfg) {
+		this.obj = obj;
+		this.cfg = cfg;
+		this.start = function () {
+			var context = this;
+			// gather all 8 of the digits
+			context.cfg.digits = context.obj.getElementsByTagName('span');
+			context.cfg.current = new Date().getSeconds();
+			// start the countdown
+			context.cfg.interval = setInterval(function () {
+				var seconds = new Date().getSeconds();
+				if (seconds !== context.cfg.current) {
+					context.cfg.current = seconds;
+					context.update();
+				}
+			}, 200);
+		};
+		this.update = function () {
+			var context = this;
+			// get the current time
+			var current = new Date();
+			// subtract the two dates
+			var rest = context.cfg.end.getTime() - current.getTime();
+			// if there is no time left
+			if (rest <= 0) {
+				// halt the counter
+				rest = 0;
+				// trigger the end event
+				context.cfg.onEnd();
+				// stop the counter
+				clearInterval(context.cfg.interval);
+			}
+			// report the days
+			var days = rest / 86400000;
+			rest = rest % 86400000;
+			var reverseDays = parseInt(days, 10).toString().split('').reverse().join('');
+			reverseDays = (reverseDays.length < 3) ? reverseDays + '00' : reverseDays;
+			reverseDays = (reverseDays.length < 2) ? reverseDays + '0' : reverseDays;
+			context.cfg.digits[0].className = 'digit_' + reverseDays.substr(2, 1);
+			context.cfg.digits[0].innerHTML = reverseDays.substr(2, 1);
+			context.cfg.digits[1].className = 'digit_' + reverseDays.substr(1, 1);
+			context.cfg.digits[1].innerHTML = reverseDays.substr(1, 1);
+			context.cfg.digits[2].className = 'digit_' + reverseDays.substr(0, 1);
+			context.cfg.digits[2].innerHTML = reverseDays.substr(0, 1);
+			// report the hours
+			var hours = rest / 3600000;
+			rest = rest % 3600000;
+			var reverseHours = parseInt(hours, 10).toString().split('').reverse().join('');
+			reverseHours = (reverseHours.length < 2) ? reverseHours + '0' : reverseHours;
+			context.cfg.digits[3].className = 'digit_' + reverseHours.substr(1, 1);
+			context.cfg.digits[3].innerHTML = reverseHours.substr(1, 1);
+			context.cfg.digits[4].className = 'digit_' + reverseHours.substr(0, 1);
+			context.cfg.digits[4].innerHTML = reverseHours.substr(0, 1);
+			// report the minutes
+			var minutes = rest / 60000;
+			rest = rest % 60000;
+			var reverseMinutes = parseInt(minutes, 10).toString().split('').reverse().join('');
+			reverseMinutes = (reverseMinutes.length < 2) ? reverseMinutes + '0' : reverseMinutes;
+			context.cfg.digits[5].className = 'digit_' + reverseMinutes.substr(1, 1);
+			context.cfg.digits[5].innerHTML = reverseMinutes.substr(1, 1);
+			context.cfg.digits[6].className = 'digit_' + reverseMinutes.substr(0, 1);
+			context.cfg.digits[6].innerHTML = reverseMinutes.substr(0, 1);
+			// report the seconds
+			var seconds = rest / 1000;
+			rest = rest % 1000;
+			var reverseSeconds = parseInt(seconds, 10).toString().split('').reverse().join('');
+			reverseSeconds = (reverseSeconds.length < 2) ? reverseSeconds + '0' : reverseSeconds;
+			context.cfg.digits[7].className = 'digit_' + reverseSeconds.substr(1, 1);
+			context.cfg.digits[7].innerHTML = reverseSeconds.substr(1, 1);
+			context.cfg.digits[8].className = 'digit_' + reverseSeconds.substr(0, 1);
+			context.cfg.digits[8].innerHTML = reverseSeconds.substr(0, 1);
+		};
+	};
+
+}(window.useful = window.useful || {}));
